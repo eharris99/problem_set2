85dbd7352e0fcbcd8be0374edea96954d2ab2612
Elise Harris <eliseharris@Elises-MacBook-Air.local>
2015-08-07 20:39:54 -0400

    Initial Commit

---
+++ hwk2.prob1/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  hwk2.prob1
+//
+//  Created by Elise Harris on 8/7/15.
+//  Copyright (c) 2015 Elise Harris. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
